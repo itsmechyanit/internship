24a51543bf66fa2b2f14927b37a18a504088cf26
Chyanit Singh <the_top_hacker@Chyanits-Laptop.local>
2023-04-17 21:59:50 +0530

    add java code for Search SearchRange MergeSortedArrays

---
+++ Leetcode/Search.java
@@ -0,0 +1,31 @@
+public class Search {
+
+    //We need O(LogN) time complexity
+    //Therefore we will use binary search here
+
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        
+        while(start<=end){
+        int mid= start+(end-start) /2;
+        if(nums[mid]==target) return mid;
+        if(nums[start]<=nums[mid]){
+            if(target<=nums[mid] && target>=nums[start]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        else{
+            if(target>=nums[mid] && target<=nums[end]){  
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        }
+        return -1;
+    }
+    
+}
